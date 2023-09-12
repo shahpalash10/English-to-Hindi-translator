7f855a04885d986934dc4df6b4053ee684a6bbd4
Palash Shah <shahpalash10@users.noreply.huggingface.co>
2023-09-12 03:25:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from transformers import pipeline
-pipe=pipeline('sentiment-analysis')
-text=st.text_area("Enter some text")
+
+
+pipe = pipeline('translation', model='Helsinki-NLP/opus-mt-en-hi')
+
+text = st.text_area("Enter some English text")
 if text:
-    out= pipe(text)
+    out = pipe(text, src_lang='en', tgt_lang='hi')
     st.json(out)
