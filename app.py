837e23c830bf518a7378577e36529cb11fc7a486
Palash Shah <shahpalash10@users.noreply.huggingface.co>
2023-09-12 05:48:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,42 +6,40 @@ import speech_recognition as sr
 # Create a translation pipeline
 pipe = pipeline('translation', model='Helsinki-NLP/opus-mt-en-hi')
 
-# Initialize the SpeechRecognition recognizer
-recognizer = sr.Recognizer()
-
-# Create a Streamlit input element for microphone input
-audio_input = st.empty()
+# Create a Streamlit input element for text input
+text_input = st.text_area("Enter some English text")
 
 # Check if the microphone input is requested
 if st.checkbox("Use Microphone for English Input"):
-    with audio_input:
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
         st.warning("Listening for audio input... Speak in English.")
-        try:
-            with sr.Microphone() as source:
-                audio = recognizer.listen(source)
-            st.success("Audio input recorded. Translating...")
+        audio = recognizer.listen(source)
+    st.success("Audio input recorded. Translating...")
 
-            # Recognize the English speech
-            english_text = recognizer.recognize_google(audio, language='en')
+    # Recognize the English speech using Google Web Speech API
+    try:
+        english_text = recognizer.recognize_google(audio, language='en')
+        text_input = st.text_area("English Input", english_text)
+    except sr.WaitTimeoutError:
+        st.warning("No speech detected. Please speak into the microphone.")
+    except sr.RequestError as e:
+        st.error(f"Could not request results from Google Speech Recognition service: {e}")
+    except sr.UnknownValueError:
+        st.warning("Speech recognition could not understand the audio.")
 
-            # Translate the English text to Hindi
-            out = pipe(english_text, src_lang='en', tgt_lang='hi')
+if text_input:
+    # Translate the English text to Hindi
+    out = pipe(text_input, src_lang='en', tgt_lang='hi')
 
-            # Extract the translation
-            translation_text = out[0]['translation_text']
-            st.text(f"English Input: {english_text}")
-            st.text(f"Hindi Translation: {translation_text}")
+    # Extract the translation
+    translation_text = out[0]['translation_text']
+    st.text(f"English Input: {text_input}")
+    st.text(f"Hindi Translation: {translation_text}")
 
-            # Convert the translated text to speech
-            tts = gTTS(translation_text, lang='hi')
-            tts.save("translated_audio.mp3")
+    # Convert the translated text to speech
+    tts = gTTS(translation_text, lang='hi')
+    tts.save("translated_audio.mp3")
 
-            # Display the audio player for listening to the speech
-            st.audio("translated_audio.mp3", format='audio/mp3')
-
-        except sr.WaitTimeoutError:
-            st.warning("No speech detected. Please speak into the microphone.")
-        except sr.RequestError as e:
-            st.error(f"Could not request results from Google Speech Recognition service: {e}")
-        except sr.UnknownValueError:
-            st.warning("Speech recognition could not understand the audio.")
+    # Display the audio player for listening to the speech
+    st.audio("translated_audio.mp3", format='audio/mp3')
