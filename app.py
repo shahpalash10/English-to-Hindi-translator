8034646533c48e06687f86dfb9858f119d33ba36
Palash Shah <shahpalash10@users.noreply.huggingface.co>
2023-09-12 04:41:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import pipeline
+from gtts import gTTS 
 
 
 pipe = pipeline('translation', model='Helsinki-NLP/opus-mt-en-hi')
@@ -8,3 +9,12 @@ text = st.text_area("Enter some English text")
 if text:
     out = pipe(text, src_lang='en', tgt_lang='hi')
     st.json(out)
+    
+    translation_text = out[0]['translation_text']
+    tts = gTTS(translation_text, lang='hi')  
+    
+  
+    tts.save("translated_audio.mp3")
+
+   
+    st.audio("translated_audio.mp3")
