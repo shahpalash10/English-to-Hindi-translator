ee07b7063e840d9d4e4c5a45627160fe47c8ea71
Palash Shah <shahpalash10@users.noreply.huggingface.co>
2023-09-12 05:13:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
 import streamlit as st
 from transformers import pipeline
-from gtts import gTTS 
+from gtts import gTTS
+import speech_recognition as sr
 
 
 pipe = pipeline('translation', model='Helsinki-NLP/opus-mt-en-hi')
 
-text = st.text_area("Enter some English text")
-if text:
-    out = pipe(text, src_lang='en', tgt_lang='hi')
-    st.json(out)
-    
-    translation_text = out[0]['translation_text']
-    tts = gTTS(translation_text, lang='hi')  
-    
-  
-    tts.save("translated_audio.mp3")
 
-   
-    st.audio("translated_audio.mp3")
+recognizer = sr.Recognizer()
+
+
+audio_input = st.empty()
+
+if st.checkbox("Use Microphone for English Input"):
+    with audio_input:
+        st.warning("Listening for audio input... Speak in English.")
+        try:
+            with sr.Microphone() as source:
+                audio = recognizer.listen(source)
+            st.success("Audio input recorded. Translating...")
+
+          
+            english_text = recognizer.recognize_google(audio, language='en')
+
+       
+            out = pipe(english_text, src_lang='en', tgt_lang='hi')
+
+            
+            translation_text = out[0]['translation_text']
+            st.text(f"English Input: {english_text}")
+            st.text(f"Hindi Translation: {translation_text}")
+
+            tts = gTTS(translation_text, lang='hi')
+            tts.save("translated_audio.mp3")
+
+            
+            st.audio("translated_audio.mp3", format='audio/mp3')
+
+        except sr.WaitTimeoutError:
+            st.warning("No speech detected. Please speak into the microphone.")
+        except sr.RequestError as e:
+            st.error(f"Could not request results from Google Speech Recognition service: {e}")
+        except sr.UnknownValueError:
+            st.warning("Speech recognition could not understand the audio.")
